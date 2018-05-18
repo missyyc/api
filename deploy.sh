177d570e79bb54a1670c9a54483fb881930d4b48
moJiXiang <mojixiang1102@gmail.com>
2018-05-18 14:58:30 +0800

    update travis.yml

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,7 @@
 #!/bin/bash
 
-package=`date '+fox-%Y-%m-%d--%H-%M.tar.gz'`
-echo $package
-
-git -C $TRAVIS_BUILD_DIR log -10 > build/_version
-echo "$package" > build/_pack
-echo "$package"
-
-
-cp -R front/web build/
-
-mkdir build/_bin
-cp -R portal/conf build/_bin/
-mv build/_bin/conf/app.prod.conf build/_bin/conf/app.conf
-cp $GOPATH/bin/portal build/_bin/
-
-echo -n 'tar ...'
-tar czvf $package build
-echo ' [done]'
-
-
-ls -lh build/
-ls -lh $package
-
-echo -n 'upload ...'
-curl -T $package -e 'upload-57d6ce1e-e41e-4bc1-afe4-f6413159cd74' -H 'Host: om-deploy.sh1a.qingstor.com' 'http://hk.opsmind.com/'
-echo ' [done]'
-
-rm -rf _bin
+git reset --hard
+git pull origin HEAD
+npm install
+pm2 stop yyc-api -f
+pm2 start /bin/server.js -n yyc-api
